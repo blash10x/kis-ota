0cf8fdb9a536f90c1202e3c72bdf101bd8c1d56f
myungsik.sung <myungsik.sung@gmail.com>
2026-01-31 17:18:54 +0900

    Initial setup

---
+++ src/main/java/blash10x/kis/ota/OtaApplication.java
@@ -0,0 +1,13 @@
+package blash10x.kis.ota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blash10x/kis/ota/OtaApplicationTests.java
@@ -0,0 +1,13 @@
+package blash10x.kis.ota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
